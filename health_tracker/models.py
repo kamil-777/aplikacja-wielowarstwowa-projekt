53ce4978f1a4c51817b2195a6b2b3c53946c43c1
kamil_7 <kamil.kwiek@student.pk.edu.pl>
2025-04-12 10:29:31 +0200

    Adding Goal model.

--- health_tracker/models.py
+++ health_tracker/models.py
@@ -1,10 +1,12 @@
 from datetime import date
 from health_tracker.extensions import db
+from datetime import datetime
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
+    goals = db.relationship('Goal', backref='user', lazy=True)
 
     # Relation 1:N – one user can have more entries
     entries = db.relationship("HealthEntry", backref="user", lazy=True)
@@ -24,3 +26,18 @@ class HealthEntry(db.Model):
 
     def __repr__(self):
         return f"<HealthEntry {self.date} | {self.steps} steps>"
+    
+class Goal(db.Model):
+    __tablename__ = 'goals'
+
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(50), nullable=False)  # np. "steps", "calories", "sleep"
+    target_value = db.Column(db.Float, nullable=False)
+    current_value = db.Column(db.Float, default=0.0)
+    is_achieved = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"<Goal {self.type} ({self.target_value})>"    
--- run.py
+++ run.py
@@ -3,5 +3,9 @@ from health_tracker import db
 
 app = create_app()
 
+# Tworzenie tabel w bazie danych
+#with app.app_context():
+#    db.create_all()
+
 if __name__ == "__main__":
     app.run(debug=True)
